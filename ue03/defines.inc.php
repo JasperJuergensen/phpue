e55411a8ee0f5c08d04ffe76d8939c56218a6925
Jasper Jürgensen <jasper.juergensen@protonmail.com>
2017-03-23 10:33:02 +0100

    new file

---
+++ ue03/defines.inc.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jasper
+ * Date: 23.03.2017
+ * Time: 10:28
+ */
+
+/**
+ * @var string NUMBER Form field constant that defines how the form field for holding a number is called
+ * (id/name).
+ */
+define("NUMBER", "number");
+
+/**
+ * @var int MIN Minimum number for the guess.
+ */
+define("MIN", 1);
+
+/**
+ * @var int MAX Maxmimum number for the guess.
+ */
+define("MAX", 100);
+
+define("GUESSES", "guesses");
+
+define("HIGHSCORE", "highscore");
+
+define("UPPER_LIMIT", "upper_limit");
+
+define("LOWER_LIMIT", "lower_limit");
+
+define("FILE", "highscore.txt");
